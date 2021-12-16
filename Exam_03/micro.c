cbffff6fcd229c9e15d04021a5fa70c423e7ac52
Delora Wulfe <dwulfe@il-b2.msk.21-school.ru>
2021-12-16 14:52:46 +0300

    stderr->stdout

--- Exam_03/micro.c
+++ Exam_03/micro.c
@@ -8,87 +8,78 @@
 
 int    error(char *message)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (message[i])
-        i++;
-    write(2, message, i);
-    return (1);
+	i = 0;
+	while (message[i])
+		i++;
+	write(1, message, i);
+	return (1);
 }
 
 int main (int argc, char **argv)
 {
-    FILE    *file;
-    float xtl, ytl, xbr, ybr;
-    int x, y, i, width = 0,
-    height = 0,
-    scan = 0;
-    char  bg, Rr, color;
-    char *canva;
+	FILE	*file;
+	float	xtl, ytl, xbr, ybr;
+	int		x, y, i, width = 0,	height = 0,	scan = 0;
+	char	bg, Rr, color;
+	char	*canva;
 
 
-    if (argc != 2)
-        return (error(ERR1));
-    if ((file = fopen(argv[1], "r")) == NULL)
-        return (error(ERR2));
-    if (fscanf(file, "%d %d %c\n", &width, &height, &bg) != 3)
-        return (error(ERR2));
-    if (width < 1 || width > 300 || height < 1 || height > 300)
-        return (error(ERR2));
-    if ((canva = malloc(sizeof(char) * width * height)) == NULL)
-        return (error(ERR2));
-    memset(canva, bg, height * width); 
-   
-    while ((scan = fscanf(file, "%c %f %f %f %f %c\n", &Rr, &xtl, &ytl, &xbr, &ybr, &color)) == 6)
-    { 
-        printf("%c %f %f %f %f %c\n", Rr, xtl, ytl, xbr, ybr, color);
-        if ((Rr != 'r' && Rr != 'R' ) || xbr <= 0 || ybr <= 0)
-            return (error(ERR2));
-        xbr = xbr + xtl;
-        ybr = ybr + ytl;
+	if (argc != 2)
+		return (error(ERR1));
+	if ((file = fopen(argv[1], "r")) == NULL)
+		return (error(ERR2));
+	if (fscanf(file, "%d %d %c\n", &width, &height, &bg) != 3)
+		return (error(ERR2));
+	if (width < 1 || width > 300 || height < 1 || height > 300)
+		return (error(ERR2));
+	if ((canva = malloc(sizeof(char) * width * height)) == NULL)
+		return (error(ERR2));
+	memset(canva, bg, height * width); 
+	while ((scan = fscanf(file, "%c %f %f %f %f %c\n", &Rr, &xtl, &ytl, &xbr, &ybr, &color)) == 6)
+	{
+		if ((Rr != 'r' && Rr != 'R' ) || xbr <= 0 || ybr <= 0)
+			return (error(ERR2));
+		xbr += xtl;
+		ybr += ytl;
+		if (xtl - (int)xtl != 0)
+			xtl += 1;
+		if (ytl - (int)ytl !=0)
+			ytl += 1;
+		y = 0;
+		while (y != height)
+		{
+			x = 0;
+			while (x != width)
+			{ 
+				if ((int)xtl <= x && x <= (int)xbr && (int)ytl <= y && y <= (int)ybr)
+				{
+					if (Rr == 'R')
+						canva[width * y + x] = color;
+					else
+						if ((int)xtl == x || (int)xbr == x || (int)ytl == y || (int)ybr == y)
+							canva[width * y + x] = color;
+				}
+				x++;
+			}
+			y++;
+		}
+	}
 
-        if (xtl - (int)xtl != 0)
-            xtl = xtl + 1;
-        if (ytl - (int)ytl !=0)
-            ytl = ytl + 1;
-        y = 0;
-       
-        while (y != height)
-        {  
-            x = 0;
-            while (x != width)
-            { 
-              
-                if ((int)xtl <= x && x <= (int)xbr && (int)ytl <= y && y <= (int)ybr)
-                { 
-                   
-                    if (Rr == 'R')
-                       canva[width * y + x] = color;
-                    if ((int)xtl == x || (int)xbr == x || (int)ytl == y || (int)ybr == y)
-                    {
-                        canva[width * y + x] = color;
-                    }
-                }
-                x++;
-            }
-            y++;
-        }
-    }
-
-    if (scan != -1)
-    {
-        printf("scan");
-        free(canva);
-        return (error(ERR2));
-    }
-    i = 0;
-    while (i < height)
-    {
-        write(1, canva + i * width, width);
-        write(1, "\n",1);
-        i++;
-    }
-    free(canva);
-    return (0);
+	if (scan != -1)
+	{
+		printf("scan");
+		free(canva);
+		return (error(ERR2));
+	}
+	i = 0;
+	while (i < height)
+	{
+		write(1, canva + i * width, width);
+		write(1, "\n", 1);
+		i++;
+	}
+	free(canva);
+	return (0);
 }
--- Exam_03/mini.c
+++ Exam_03/mini.c
@@ -1,90 +1,78 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-# define ERR1 "Error: argument\n"
-# define ERR2 "Error: Operation file corrupted\n"
+#define Err1 "Error: argument"
+#define Err2 "Error: Operation file corrupted"
 
-int error(char *err)
+int error(char *mes)
 {
-	int i = 0;
-	while (err[i] != '\0')
-		i++;
-	write(1, err, i);
-	return (1);
+    int i = 0;
+    while (mes[i])
+        i++;
+    write(1, mes, i);
+    return (1);
 }
 
-
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	FILE *file;
+    char bg, Cc, color;
+    int width, height, i, scan;
+    float xb, yb, radius, dist;
+    int xa, ya;
+    FILE *file;
+    char *canva;
 
-	int wcanv, hcanv;
-	char bg;
+    file = NULL;
+    if (argc != 2)
+        return (error(Err1));
+    if ((file = fopen(argv[1], "r")) == NULL)
+        return (error(Err2));
+    if (fscanf(file, "%d %d %c\n", &width, &height, &bg) != 3)
+        return (error(Err2));
+    if (width < 1 || width > 300 || height < 1 || height > 300)
+        return (error(Err2));
+    if ((canva = malloc(sizeof(char) * height * width)) == NULL)
+        return (error(Err2));
+    memset(canva, bg, sizeof(char) * width * height);
 
-	char *canvas;
+    while ((scan = fscanf(file, "%c %f %f %f %c\n", &Cc, &xb, &yb, &radius, &color)) == 5)
+    {
+        if (radius <= 0 || (Cc != 'c' && Cc != 'C'))
+            break ;
+        ya = 0;
+        while (ya < height)
+        {
+            xa = 0;
+            while (xa < width)
+            {
+                dist = sqrtf(((float)xa - xb) * ((float)xa - xb) + ((float)ya - yb)* ((float)ya - yb));
+                if (dist <= radius)
+                {
+                    if ( dist + 1 > radius && Cc == 'c')//radius - dist < 1.000000
+                        canva[width * ya + xa] = color;
+                    if (Cc == 'C')
+                        canva[width * ya + xa] = color;
+                }
+                xa++;
+            }
+            ya++;
+        }
+    }
 
-	int scan, xa, ya;
-
-	char Cc;
-	float xb, yb, radius;
-	char color;
-
-	float dist;
-
-	int i = 0;
-	if (ac != 2)
-		return (error(ERR1));
-	else if(!(file = fopen(av[1], "r")))
-		return (error(ERR2));
-	else if ((fscanf(file, "%d %d %c\n", &wcanv, &hcanv, &bg)) != 3)
-		return (error(ERR2));
-	else if ((!(0 < wcanv && wcanv <= 300)) || (!(0 < hcanv && hcanv <= 300)))
-		return (error(ERR2));
-	else if ((!(canvas = (char *)malloc(sizeof(char) * wcanv * hcanv))))
-		return (error(ERR2));
-	else
-		memset(canvas, bg, wcanv * hcanv);
-	while ((scan = fscanf(file, "%c %f %f %f %c\n", &Cc, &xb, &yb, &radius,
-						  &color)) == 5)
-	{
-		if (radius <= 0 || (Cc != 'c' && Cc != 'C'))
-			break;
-		ya = 0;
-		while (ya != hcanv)
-		{
-			xa = 0;
-			while (xa != wcanv)
-			{
-				dist = sqrtf(((float)xa - xb) * ((float)xa - xb) +
-							 ((float)ya - yb) * ((float)ya - yb));
-				if (dist <= radius)
-				{
-					if (radius - dist < 1.00000000 && Cc == 'c')
-						canvas[ya * wcanv + xa] = color;
-					if (Cc == 'C')
-						canvas[ya * wcanv + xa] = color;
-				}
-				xa++;
-			}
-			ya++;
-		}
-
-	}
-	if (scan != -1)
-	{
-		free(canvas);
-		return(error(ERR2));
-	}
-	while (i != hcanv)
-	{
-		write(1, canvas + i * wcanv, wcanv);
-		write(1, "\n", 1);
-		i++;
-	}
-	free(canvas);
-	fclose(file);
-	return(0);
+    if (scan != -1)
+    {
+        free(canva);
+        return(error(Err2));
+    }
+        i = 0;
+    while (i < height)
+    {
+        write(1, canva + width * i, width);
+        write(1, "\n", 1);
+        i++;
+    }
+    return (0);
 }
